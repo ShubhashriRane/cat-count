609eb81bd5428b8d94a56003ad0ca4abc0094011
ShubhashriRane <34337322+ShubhashriRane@users.noreply.github.com>
2017-12-07 16:41:43 +0530

    Add files via upload

---
+++ catdetect_real.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+'''
+face detection using haar cascades
+
+USAGE:
+    facedetect.py [--cascade <cascade_fn>] [--nested-cascade <cascade_fn>] [<video_source>]
+'''
+
+# Python 2/3 compatibility
+from __future__ import print_function
+
+import numpy as np
+import cv2
+
+# local modules
+from video import create_capture
+from common import clock, draw_str
+
+
+def detect(img, cascade):
+    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30),
+                                     flags=cv2.CASCADE_SCALE_IMAGE)
+    if len(rects) == 0:
+        return []
+    rects[:,1:] += rects[:,:1]
+    return rects
+
+def draw_rects(img, rects, color):
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+
+if __name__ == '__main__':
+    import sys, getopt
+    print(__doc__)
+
+    args, video_src = getopt.getopt(sys.argv[1:], '', ['cascade='])
+    try:
+        video_src = video_src[0]
+    except:
+        video_src = 0
+    args = dict(args)
+    cascade_fn = args.get('--cascade', "../../data/haarcascades/haarcascade_frontalcatface.xml")
+
+    cascade = cv2.CascadeClassifier(cascade_fn)
+   
+    cam = create_capture(video_src)
+
+    while True:
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.equalizeHist(gray)
+
+        
+        rects = detect(gray, cascade)
+        vis = img.copy()
+       
+        rects = cascade.detectMultiScale(gray, scaleFactor=1.3,
+	minNeighbors=10, minSize=(75, 75))
+
+
+        for (i, (x, y, w, h)) in enumerate(rects):
+            cv2.rectangle(vis, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            cv2.putText(vis, "Cat #{}".format(i + 1), (x, y - 10),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
+	
+        cv2.imshow('cat', vis)
+
+        if cv2.waitKey(5) == 27:
+            break
+    cv2.destroyAllWindows()
