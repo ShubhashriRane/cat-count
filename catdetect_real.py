26062eba543a1ce711f9c9418835869e4e1a111c
ShubhashriRane <34337322+ShubhashriRane@users.noreply.github.com>
2017-12-08 23:52:47 +0530

    Update catdetect_real.py

--- catdetect_real.py
+++ catdetect_real.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+# Python 2/3 compatibility
 from __future__ import print_function
 from firebase import firebase
 
@@ -16,7 +17,7 @@ def detect(img, cascade):
                                      flags=cv2.CASCADE_SCALE_IMAGE)
     if len(rects) == 0:
         return []
-    rects[:,1:] += rects[:,:1]
+    rects[:,2:] += rects[:,:2]
     return rects
 
 def draw_rects(img, rects, color):
@@ -27,44 +28,49 @@ if __name__ == '__main__':
     import sys, getopt
     print(__doc__)
 
-    args, video_src = getopt.getopt(sys.argv[1:], '', ['cascade='])
+    args, video_src = getopt.getopt(sys.argv[1:], '', ['cascade=', 'nested-cascade='])
     try:
         video_src = video_src[0]
     except:
         video_src = 0
     args = dict(args)
     cascade_fn = args.get('--cascade', "../../data/haarcascades/haarcascade_frontalcatface.xml")
+    nested_fn  = args.get('--nested-cascade', "../../data/haarcascades/haarcascade_frontalcatface_extended.xml")
 
     cascade = cv2.CascadeClassifier(cascade_fn)
-   
-    cam = create_capture(video_src)
-    count=0
+    nested = cv2.CascadeClassifier(nested_fn)
 
+    cam = create_capture(video_src, fallback='synth:bg=../data/lena.jpg:noise=0.05')
+    count=0
     while True:
         ret, img = cam.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         gray = cv2.equalizeHist(gray)
 
-        t=clock()
+        t = clock()
         rects = detect(gray, cascade)
         vis = img.copy()
-       
-        rects = cascade.detectMultiScale(gray, scaleFactor=1.3,
-	minNeighbors=10, minSize=(75, 75))
+        draw_rects(vis, rects, (0, 255, 0))
+        
+        if not nested.empty():
+            for x1, y1, x2, y2 in rects:
+                roi = gray[y1:y2, x1:x2]
+                vis_roi = vis[y1:y2, x1:x2]
+                subrects = detect(roi.copy(), nested)
+                draw_rects(vis_roi, subrects, (255, 0, 0))
+        dt = clock() - t
 
-
-        for (i, (x, y, w, h)) in enumerate(rects):
-            cv2.rectangle(vis, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            cv2.putText(vis, "Cat #{}".format(i + 1), (x, y - 10),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
-	
-        cv2.imshow('cat', vis)
-
+        draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
+        cv2.imshow('facedetect', vis)
+        
         if cv2.waitKey(5) == 27:
             break
     cv2.destroyAllWindows()
-    firebase = firebase.FirebaseApplication('https://mitdb-7e16b.firebaseio.com/')
-    data = {'Cat' : count }
-
-    result= firebase.put('https://mitdb-7e16b.firebaseio.com','/mitdb-7e16b',data)
-    print(result)    
+    count=len(rects)
+    print ("no of cats" )
+    print (count)
+    firebase = firebase.FirebaseApplication('https://sd14cs105.firebaseio.com/Cat')
+    data = {'Cats':count}
+    
+    result = firebase.put('https://sd14cs105.firebaseio.com/Cat','sd14cs105',data)
+    print(result)
